a299563a4a1e4bd2d6d0b06870f6ee30f5a75874
Sriramkod <kodipakasriram@gmail.com>
2019-07-23 16:01:23 +0530

    Front end of splash timeout is updated

--- app/src/main/java/com/example/ksriram/newproject/HomeActivity.java
+++ app/src/main/java/com/example/ksriram/newproject/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.ksriram.newproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,29 +15,39 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity{
+    boolean connected = false;
+    EditText editText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homeactivity);
         Button startButton = (Button)findViewById(R.id.button);
-        final EditText editText = (EditText)findViewById(R.id.acess);
+        editText = (EditText)findViewById(R.id.acess);
+
+        //boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        }
+        else
+            connected = false;
 
         startButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
 
             public void onClick(View v) {
-                EditText text = (EditText)findViewById(R.id.acess);
-                if(text.equals(123)){
-                    Toast.makeText(HomeActivity.this, "Enter the acess key", Toast.LENGTH_LONG).show();
+                //EditText text = (EditText)findViewById(R.id.acess);
+               String edit = editText.getText().toString();
+                if(connected){
+                    Intent intent = new Intent(HomeActivity.this, QuizActivity.class);
+                    intent.putExtra("acess",edit);
+                    startActivity(intent);
                 }
                 else {
-                    String acessKey = editText.getText().toString();
-                    Intent intent = new Intent(HomeActivity.this, QuizActivity.class);
-
-                    intent.putExtra("acessKey", acessKey);
-                    startActivity(intent);
-
+                    Toast.makeText(HomeActivity.this, "Please check your INTERNET connection ", Toast.LENGTH_LONG).show();
                 }
 
                 /*Intent intent = new Intent(HomeActivity.this, QuizActivity.class);
